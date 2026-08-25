8b07ba2476ab70250c98f8e83e420f4a0d8a2e80
Fanboynz <mp3geek@gmail.com>
2026-08-25 15:31:06 +1200

    Bump build numbers for the next 10.5.1 staging build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         applicationId = "com.noop.whoop"
         minSdk = 26
         targetSdk = 34
-        versionCode = 357
+        versionCode = 358
         versionName = "10.5.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
